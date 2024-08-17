48f427ae22fa2c27b5fbee05da7a855f6f4443ac
bojorge <bojorgearayamanuel@gmail.com>
2024-08-16 20:59:03 -0600

    Proof of concepts completed

--- Charts/charts.py
+++ Charts/charts.py
@@ -9,35 +9,35 @@ def plot_usage(libraries, ram_usage, user_cpu_usage, system_cpu_usage, total_exe
     
     # Gráfico de uso de RAM
     axs[0].bar(libraries, ram_usage, color='skyblue')
-    axs[0].set_ylabel('Uso de RAM (KB)')
+    axs[0].set_ylabel('Uso de \nRAM (KB)')
     axs[0].tick_params(axis='x', rotation=45)
     
     # Gráfico de uso de CPU en modo usuario
     axs[1].bar(libraries, user_cpu_usage, color='lightgreen')
-    axs[1].set_ylabel('CPU modo usuario (s)')
+    axs[1].set_ylabel('CPU modo \nusuario (s)')
     axs[1].tick_params(axis='x', rotation=45)
     
     # Gráfico de uso de CPU en modo sistema
     axs[2].bar(libraries, system_cpu_usage, color='salmon')
-    axs[2].set_ylabel('CPU modo sistema (s)')
+    axs[2].set_ylabel('CPU modo \nsistema (s)')
     axs[2].tick_params(axis='x', rotation=45)
 
     # Gráfico de tiempo total de ejecución
     axs[3].bar(libraries, total_execution_time, color='mediumpurple')
-    axs[3].set_ylabel('Tiempo total de ejecución (s)')
+    axs[3].set_ylabel('Tiempo de \nejecución (s)')
     axs[3].tick_params(axis='x', rotation=45)
     
     # Ajustar el espaciado para que no se sobrepongan las etiquetas
-    plt.subplots_adjust(hspace=0.9, top=0.93, bottom=0.12, left=0.1, right=0.95)
+    plt.subplots_adjust(hspace=1.5, top=0.93, bottom=0.12, left=0.1, right=0.95)
     
     # Título de la ventana
-    fig.suptitle('Shared memory - Creator', fontsize=15)
+    fig.suptitle('READER', fontsize=15)
     
     # Mostrar gráficos
     plt.show()
 
 # Ejemplo de uso
-libraries = ['POSIX', 'Boost.Interprocess', 'POCO']
+libraries = ['Boost.InterProcess', 'POCO', 'POSIX']
 ram_usage = [0, 0, 0]  # en KB
 user_cpu_usage = [0.0, 0.0, 0.0]  # en segundos
 system_cpu_usage = [0.0, 0.0, 0.0]  # en segundos
