3b184c37a51b53057605aa416faa9b2651469b36
bojorge <bojorgearayamanuel@gmail.com>
2024-08-26 01:54:27 -0600

    -Ofast

--- IPC/message_passing/POSIX/sockets.cpp
+++ IPC/message_passing/POSIX/sockets.cpp
@@ -31,37 +31,31 @@ void start_server(const char* port) {
     struct sockaddr_in serv_addr, cli_addr;
     int n;
 
-    // Variables para almacenar los máximos valores
     long maxRAMUsage = 0;
     double maxUserCPU = 0.0;
     double maxSystemCPU = 0.0;
 
-    // Crear socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         perror("ERROR opening socket");
         exit(1);
     }
 
-
-    // Configurar la dirección del servidor
-    memset((char *) &serv_addr, 0, sizeof(serv_addr));
+    memset(&serv_addr, 0, sizeof(serv_addr));
     portno = atoi(port);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
 
-    // Enlazar el socket
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         perror("ERROR on binding");
-        close(sockfd); // cerrar el socket en caso de error
+        close(sockfd);
         exit(1);
     }
 
-    // Escuchar conexiones entrantes
     if (listen(sockfd, 5) < 0) {
         perror("ERROR on listen");
-        close(sockfd); // cerrar el socket en caso de error
+        close(sockfd);
         exit(1);
     }
 
@@ -72,39 +66,36 @@ void start_server(const char* port) {
         newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
         if (newsockfd < 0) {
             perror("ERROR on accept");
-            close(sockfd); // cerrar el socket principal en caso de error
+            close(sockfd);
             exit(1);
         }
 
-        // Leer el mensaje del cliente
-        memset(buffer, 0, 256);
-        n = read(newsockfd, buffer, 255);
+        n = read(newsockfd, buffer, sizeof(buffer) - 1);
         if (n < 0) {
             perror("ERROR reading from socket");
-            close(newsockfd); // cerrar el socket del cliente en caso de error
-            continue; 
+            close(newsockfd);
+            continue;
         }
 
         std::cout << "\n * Mensaje recibido >>> " << buffer << std::endl;
 
-        long ramUsage = getRAMUsage();
-        double userCPU, systemCPU;
-        getCPUUsage(userCPU, systemCPU);
+        if (message_count < num_iterations) {
+            long ramUsage = getRAMUsage();
+            double userCPU, systemCPU;
+            getCPUUsage(userCPU, systemCPU);
 
-        if (ramUsage > maxRAMUsage) maxRAMUsage = ramUsage;
-        if (userCPU > maxUserCPU) maxUserCPU = userCPU;
-        if (systemCPU > maxSystemCPU) maxSystemCPU = systemCPU;
+            if (ramUsage > maxRAMUsage) maxRAMUsage = ramUsage;
+            if (userCPU > maxUserCPU) maxUserCPU = userCPU;
+            if (systemCPU > maxSystemCPU) maxSystemCPU = systemCPU;
+        }
 
-        // Cerrar el socket del cliente
         close(newsockfd);
-        message_count++;
+        ++message_count;
     }
 
-    // Cerrar el socket principal del servidor
     std::cout << "\n *** Se han recibido " << num_iterations << " mensajes. Cerrando servidor..." << std::endl;
     close(sockfd);
 
-    // Mostrar el uso máximo de recursos
     std::cout << "RAM: " << maxRAMUsage << " KB" << std::endl;
     std::cout << "CPU usuario: " << maxUserCPU << " s" << std::endl;
     std::cout << "CPU sistema: " << maxSystemCPU << " s" << std::endl;
@@ -125,25 +116,25 @@ void send_message(const char* server_ip, const char* port, const char* message) 
     server = gethostbyname(server_ip);
     if (server == nullptr) {
         std::cerr << "ERROR, no such host" << std::endl;
-        close(sockfd); 
+        close(sockfd);
         exit(1);
     }
 
-    std::memset(&serv_addr, 0, sizeof(serv_addr));
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    std::memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
 
     if (connect(sockfd, reinterpret_cast<struct sockaddr*>(&serv_addr), sizeof(serv_addr)) < 0) {
         std::cerr << "ERROR connecting" << std::endl;
-        close(sockfd); 
+        close(sockfd);
         exit(1);
     }
 
     n = write(sockfd, message, std::strlen(message));
     if (n < 0) {
         std::cerr << "ERROR writing to socket" << std::endl;
-        close(sockfd); 
+        close(sockfd);
         exit(1);
     }
 
